0c2af52f6169f4b559daa01ce4f4c4178f60f6d4
GopiChandAkkala <akkalagopi@gmail.com>
2024-01-13 10:25:41 +0530

    added webtier appt tier

---
+++ app-tier/DbConfig.js
@@ -0,0 +1,6 @@
+module.exports = Object.freeze({
+    DB_HOST : 'database-1-instance-1.ctcu2uawen09.us-east-1.rds.amazonaws.com',
+    DB_USER : 'admin',
+    DB_PWD : 'password',
+    DB_DATABASE : 'webappdb'
+});
